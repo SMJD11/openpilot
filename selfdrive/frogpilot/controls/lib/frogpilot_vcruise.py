d8e8a71a7a4bf6609d5636f81462b9f0ed4a415e
FrogAi <91348155+FrogAi@users.noreply.github.com>
2025-01-07 16:42:47 -0700

    MTSC Rewrite

--- selfdrive/frogpilot/controls/lib/frogpilot_vcruise.py
+++ selfdrive/frogpilot/controls/lib/frogpilot_vcruise.py
@@ -59,7 +59,8 @@ class FrogPilotVCruise:
 
     # Pfeiferj's Map Turn Speed Controller
     if frogpilot_toggles.map_turn_speed_controller and v_ego > CRUISING_SPEED and carControl.longActive:
-      self.mtsc_target = clip(self.mtsc.target_speed(v_ego, carState.aEgo, frogpilot_toggles), CRUISING_SPEED, v_cruise)
+      mtsc_speed = ((TARGET_LAT_A * frogpilot_toggles.turn_aggressiveness) / (self.mtsc.get_map_curvature(v_ego) * frogpilot_toggles.curve_sensitivity))**0.5
+      self.mtsc_target = clip(mtsc_speed, CRUISING_SPEED, v_cruise)
 
       if not self.frogpilot_planner.road_curvature_detected and frogpilot_toggles.mtsc_curvature_check:
         self.mtsc_target = v_cruise
